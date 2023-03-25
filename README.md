122152720276a1af7960f6b36a3380d39a133129
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-03-26 02:16:35 +0900

    add preview

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Odin-Landingpagemark2-AMA
 A more refined? version of the Oding project Landing page. with my own designs etc added to it.
+
+
+preview : https://evilkeil.github.io/Odin-Landingpagemark2-AMA/
---
+++ images/Loop practice_files/loop.js
@@ -0,0 +1,13 @@
+// 5x multiplication table till 5 * 10
+
+
+function multiply(){
+    for(let i = 1; i < 11; i++){
+        console.log(`${5} X ${i}  = ` + (5*i))
+
+    }
+    console.log("thats the end of the 5X table till 5 X 10");
+}
+
+
+multiply();
